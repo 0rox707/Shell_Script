570b448fff75614186f5c375e65e6cf56abfc5f3
JoãoG <J.Gomes0963@gmail.com>
2024-10-31 12:44:47 -0300

    Modificando arquivos Criar e Commit

--- Commit.sh
+++ Commit.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 clear
-echo "Fazer commit"$'\n'"--------------"$'\n'"Mensagem de commit"
+echo "Fazer commit"$'\n'"--------------"$'\n'"Mensagem de commit"$'\n'"Para cancelar o Commit digite //c"
 read msg
 clear
+if [ $msg = "//c" ];then 
+    i=0
+    echo -n "encerrando"
+    while [ $i -lt 2 ];do
+    i=$(( $i + 1 ))
+    echo -n "."
+    sleep 0.5
+    done
+    echo "."
+    exit
+fi
 git commit -m "$msg"
 exit
--- Del.sh
+++ Del.sh
@@ -7,7 +7,33 @@ read nome
 
 if [ -e $nome ];then 
     rm $nome 
-    git rm --cached $nome
 else
     echo "Este arquivo não existe!"
 fi
+
+while [ true ];do 
+    echo "Remover do vercionamento?"$'\n'"s/n"
+
+    read sc
+
+    if [ $sc = "S" ] || [ $sc = "s" ];then
+        git rm $nome
+        clear
+        echo "Arquivo $nome removido do versionamento"
+        exit
+    elif [ $sc = "N" ] || [ $sc = "n" ];then
+        echo -n "Encerrando"
+            i=0
+            while [ $i -lt 2 ];do
+            i=$(($i+1))
+            echo -n "."
+            sleep 0.8
+            done
+            echo "."
+            exit
+    else
+        echo "Opção invalida!"
+        sleep 0.85
+        clear
+    fi
+done
