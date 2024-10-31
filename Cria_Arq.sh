229866d838042ecdb40481d5fa2bb4a1a05ae503
JoãoG <J.Gomes0963@gmail.com>
2024-10-31 12:22:07 -0300

    Comitando a modificação do Criar aquivo

--- Cria_Arq.sh
+++ Cria_Arq.sh
@@ -2,11 +2,39 @@
 clear
 echo "--------"$'\n'"Criar arquivos"$'\n'"------------"$'\n'"Qual o nome do arquivo: "
 read nome
-
+clear
 if [ -e $nome ];then 
     echo "Este arquivo ja existe!"
 else
     echo "Criando $nome!"
     touch $nome
-    git add $nome
+    
 fi
+
+
+while [ true ] ;do  
+    clear
+    echo "Gostaria de adicionar $nome ao versonamento git?"$'\n'"s/n"
+    read c
+
+    clear
+    if [ $c = "S" ] || [ $c = "s" ];then
+        echo "Adicionando $nome ao versonamento!"
+        git add $nome
+        exit
+    elif [ $c = "N" ] || [ $c = "n" ];then
+        echo -n "Encerrando"
+        i=0
+        while [ $i -lt 2 ];do
+        i=$(($i+1))
+        echo -n "."
+        sleep 0.8
+        done
+        echo "."
+        exit
+    else
+        echo "Opção invalida!"
+        sleep 2
+    fi
+
+done
