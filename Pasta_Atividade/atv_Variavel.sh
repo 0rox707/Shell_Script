36e190e9e14dda16ce13e0f84bf29e2819738683
JoãoG <J.Gomes0963@gmail.com>
2024-11-07 14:45:16 -0300

    Commit de atividade

---
+++ Pasta_Atividade/atv_Variavel.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# ============ exercicios de variaveis =========
+
+# clear
+# echo "Exercícios de variaveis"$'\n'"exercício 1"$'\n'\
+#         "Digite seu nome: "
+# read nome
+# echo "Seu nome: $nome"$'\n'$'\n'"-------------------------"
+
+
+
+# ---------------- Execicio 2 -----------------------------------
+
+# echo "Exercício 2"$'\n'"Digite um numero: "
+# read n1
+# echo "DIgite outro numero: "
+# read n2
+# soma=$(( $n2 + $n1 ))
+# echo "Soma de $n1 + $n2 = $soma"$'\n'$'\n'
+
+#--------------------- Exercicio 3 --------------------------------
+
+# echo "Exercício 3"
+
+
+
+
+# -------------------- Exerciocio 4 -------------------------------
+
+# echo "Exercício 4"$'\n'"Digite sua idade: "
+# read id
+# echo "Você tem $id de idade!"
+
+# --------------------- Exercicio 5 -------------------
+
+# echo "Exerciocio 5"$'\n'"Ola $USER!"
+
+# ===================== Execicio do if ===============
+
+# echo "Atividade 1"$'\n'"Digite um numero: "
+# read n
+# if [ $n -gt 0 ];then
+#     clear
+#     echo "$n e um numero positivo!"
+# elif [ $n -lt 0 ];then
+#     clear
+#     echo "$n e um numero E negativo!"
+# else
+#     clear
+#     echo "$n e neutro"
+# fi
+
+# ------------------- Exercicio 2 -----------------
+
+# echo "Atividade 2"$'\n'"Digite sua idade: "
+# read n
+# if [ $n -lt 18 ];then
+#     clear 
+#     echo "Voçê possue $n."$'\n'"E menor de idade!"
+#     else
+#         echo "Voçê possue $n."$'\n'"E maior de idade"
+# fi
+
+# ------------------ Exercicio 3 --------------------
+
+# echo "Atividade 3"$'\n'"(Este exercicio usa argumentos por praticidade caso queira)"$'\n'"--------------------"
+# if [ -e $1 ];then
+#     echo "$1 Existe"
+# else
+#     echo "Não existe no arquivo neste diretorio!"
+
+# fi
+# echo "------------------"
+
+
+
+
+
+
+
+
+
+
+
+
