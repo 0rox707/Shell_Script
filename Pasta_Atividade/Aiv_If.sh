045ddd6a362ae00b965759dc42dfb231e4c633ac
JoãoG <J.Gomes0963@gmail.com>
2024-11-11 19:51:58 -0300

    Modificando atividade

--- Pasta_Atividade/Aiv_If.sh
+++ Pasta_Atividade/Aiv_If.sh
@@ -27,11 +27,38 @@
 
 # ------------------ Exercicio 3 --------------------
 
-# echo "Atividade 3"$'\n'"(Este exercicio usa argumentos por praticidade caso queira)"$'\n'"--------------------"
-# if [ -e $1 ];then
-#     echo "$1 Existe"
-# else
-#     echo "Não existe no arquivo neste diretorio!"
+# #( Passado por parametro )
 
+# echo "Atividade 3"$'\n'"(Este exercicio usa argumentos por praticidade caso queira)"$'\n'"--------------------"
+# if [ -n "$1" ];then
+#     if [ -e "$1" ];then
+#         echo "O $1 Existe"
+#     else
+#         echo "Não existe no arquivo neste diretorio!"
+#     fi
+# else
+#     echo "Parametro não passado!"
 # fi
 # echo "------------------"
+
+# ------------------- Exercicio 4 ---------------------
+
+# Crie um script que peça ao usuário para digitar um número e, em seguida, determine se ele é par ou ímpar.
+
+# echo "Exercicio 4"$'\n'"------------------------------"
+# echo "Digite um numero:"
+# read n
+# resto=$(( $n % 2))
+# if [ $resto -eq 0 ];then
+#     echo "O $n e par"
+# else
+#     echo "O $n e impar"
+# fi
+
+# ----------------- Execicio 5 -------------------------
+
+
+
+
+# Escreva um script que pergunte ao usuário o dia da semana e exiba se é um dia útil ou um dia de folga.
+
