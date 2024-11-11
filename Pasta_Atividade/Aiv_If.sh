9fb42141494ffc0bceea222317a089ed0c06f214
JoãoG <J.Gomes0963@gmail.com>
2024-11-11 19:12:28 -0300

    Adicionando atividades

---
+++ Pasta_Atividade/Aiv_If.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# -------------- Exercicio 1-----------------------
+# echo "Atividade 1"$'\n'"Digite um numero: "
+# read n
+# if [ $n -gt 0 ];then
+#     clear
+#     echo "$n e um numero positivo!"
+# elif [ $n -lt 0 ];then
+#     clear
+#     echo "$n e um numero E negativo!"
+# else
+#     clear
+#     echo "$n e neutro"
+# fi
+
+# ------------------- Exercicio 2 -----------------
+
+# echo "Atividade 2"$'\n'"Digite sua idade: "
+# read n
+# if [ $n -lt 18 ];then
+#     clear 
+#     echo "Voçê possue $n."$'\n'"E menor de idade!"
+#     else
+#         echo "Voçê possue $n."$'\n'"E maior de idade"
+# fi
+
+# ------------------ Exercicio 3 --------------------
+
+# echo "Atividade 3"$'\n'"(Este exercicio usa argumentos por praticidade caso queira)"$'\n'"--------------------"
+# if [ -e $1 ];then
+#     echo "$1 Existe"
+# else
+#     echo "Não existe no arquivo neste diretorio!"
+
+# fi
+# echo "------------------"
