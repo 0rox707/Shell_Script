7b8f0b54c572754391294c2ecea9e437a5d7b6d2
0rox707 <j.gomes0963@gmail.com>
2024-11-14 15:29:51 -0300

    Salvando mudaça

--- Cria_Arq.sh
+++ Cria_Arq.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
+
+# Cria_Arq.sh - Cria arquivos
+
+# GitHub:         https://github.com/0rox707
+# Autor:          João Victor Gomes
+# =--------------------------------------------------------=
+# Cria arquivos e adiciona no versionamento git.
+
+# Exemplo: 
+#     ./Cria_Arq.sh Nome_arqiuvo
+# =--------------------------------------------------------=
+# Hitórico:
+# versão 1.1 (atual)
+# =--------------------------------------------------------=
+# Testado em:
+#     bash 3.2.57(1)
+# ==========================================================
+
+
+
 clear
 echo "--------"$'\n'"Criar arquivos"$'\n'"------------"
 if [ -e $1 ];then 
     echo "Este arquivo ja existe!"
+elif [ -z $1 ];then 
+    l=0
+    # while 0 -eq $l;do
+    echo "Digite o nome do arquivo: "$'\n'"(Caso queira sair utilize //c )"
+    read r
+    # if [ "$r" = "//c"  ] || [ "$r" = "//C" ];then
+    #     echo "encerrando"
+    #     exit
+    #     elif [ -n "$r" ];then 
+    #         $1="$r"
+    #         $l=1
+    #     else
+    #         echo "Opção invalida"
+    # fi
+    # done
+    
 else
     echo "Criando $1!"
     touch $1
